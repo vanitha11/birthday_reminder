ae965fd70cc171a826437808b11ee7aa3fc481dd
vanitha11 <vanivenkat1@gmail.com>
2021-03-16 07:57:57 +0530

    Updates

---
+++ precache-manifest.df37da983542e78c69c97cb95c21f776.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01bfbd022c199fc5b837e80f3f0a9d62",
+    "url": "/birthday_reminder/index.html"
+  },
+  {
+    "revision": "46b0d6d9aa53be00c29b",
+    "url": "/birthday_reminder/static/css/main.10d6a708.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/birthday_reminder/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthday_reminder/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46b0d6d9aa53be00c29b",
+    "url": "/birthday_reminder/static/js/main.c1d0e4b9.chunk.js"
+  },
+  {
+    "revision": "5c461eb1782bc71025aa",
+    "url": "/birthday_reminder/static/js/runtime-main.9de7cee4.js"
+  }
+]);
